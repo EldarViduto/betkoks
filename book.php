e72c9362fa5852d50c5cc625c786fdbd53144778
judu-dus <jurga.duseviciene@gmail.com>
2020-03-06 20:27:03 +0200

    ...

--- book.php
+++ book.php
@@ -1,25 +1,45 @@
-
 <?php include "header.php" ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <body>
-   
-<div class=" col s12 s4 center-align" >
-<img src="images/9786094840869.jpg"width="200" height="320">
-    <h3>Barbora Radvilaitė</h3>
-  <h4>Aprašymas</h4>
-  
-  
-  <p><font size="1">Turbūt visi sutinka, kad pati žymiausia visų laikų lietuvė yra Barbora Radvilaitė. Nuo jos gimimo dienos praėjo jau penki šimtai metų, o moters asmenybė vis dar nepamiršta. Priešingai – Barbora apipinta keisčiausias gandais, legendomis, neįtikėtinomis istorijomis apie jos gyvenimą, meilę Žygimantui Augustui bei paslaptingą mirtį. Lenkų istorikas Zbigniew Kuchowicz penkių šimtų puslapių knygoje „Barbora Radvilaitė“ dalinasi savo provokuojančiu tyrimu apie legendinės asmenybės gyvenimą.
-Barboros Radvilaitės vardu vadiname miestų gatves, apie šią moterį statomi filmai, spektakliai, rašomos knygos. Regis Barbora nebuvo karvedė, realiai nevaldė šalies, bet vistiek tapo mūza daugeliui menininkų ir tyrinėjimo objektu žymiausiems istorikams.
-Žygimanto Augusto ir Barboros Radvilaitės meilės istorija išskirtinė ir iki pat šių dienų žadina smalsumą. Nei jų gyvenimo, nei dar vėlesniais laikais didikams ir karaliams nebuvo įprasta tuoktis iš meilės. Visgi ši pora nugalėjo tradicinį pasipriešinimą ir sukūrė šeimą vedini vien meilės jausmo. Tiesa, taip Barbora susilaukė ypatingai daug neapykantos iš visuomenės – moteris buvo vadinama ragana, kekše, intrigante ir kitais nemaloniais epitetais. O gal ji tokia ir buvo? – provokuoja autorius. Arba atvirkščiai, gal Barbora tiesiog tapo istorinių aplinkybių auka?
-Lenkų istorikas Zbigniew Kuchowicz pirmasis pabandė Barborą Radvilaitę vertinti objektyviai atsižvelgdamas į istorines aplinkybes bei tuo metu vyravusią politinę aplinką. „Barbora Radvilaitė“ – labai detalus ir įtraukiantis tyrimas, neįprasta istorinės asmenybės biografija, o taip pat ir meilės bei nepaprasto gyvenimo istorija.
-Zbigiew Kuchowicz gimė 1927-tais metais. Istorikas nuo 1956-tų metų dėstė Lodzės universitete. Pagrindinės jo tyrinėjimų sritys buvo XVI-XVIII amžiaus Lenkijos visuomenės kultūra ir seksualiniai papročiai, maistas, sveikata, moterų vaidmuo Lenkijos karalystės ir Lietuvos Didžiosios Kunigaikštystės istorijoje. Šiomis temomis Zbigniew Kuchowicz išleido aštuonias studijas. Autoriaus knygos pasižymi ir patrauklia tematika, ir literatūriniu meistriškumu. Išskirtinis tyrimas apie Barborą Radvilaitę pirmą kartą pasirodė 1976-tais metais. Lietuvių kalba pirmasis „Barboros Radvilaitės“ leidimas pasirodė 1991-ais metais. Istorikas Zbigniew Kuchowicz mirė 1991-ais metais palikęs skaitytojams galimybę į istoriją pažvelgti kitu – neįprastu – kampu.
-Iš lenkų kalbos vertė Romualdas Petraitis
-</font></p>
 
 
-</div>
+    <div class="col s12 m4 left-align container vspace ">
 
+        <div class="row">
+            <div class="col s6 m4">
+                <img src="images/9786094840869.jpg" width="200" height="290">
+            </div>
+            <div class="col s6 m4 bookinfo center-align">
+                <p> Leidėjas: Obuolys <br>Išleidimo metai: 2020<br>Formatas: 14x21, kieti viršeliai<br>Puslapių skaičius: 464 </p>
+                <a href="tekstai/BarboraRadvilaite.pdf">Skaitykite ištrauką</a>
+
+            </div>
+            <div class="right-align">
+                <button class="btn waves-effect waves-light" type="NORIU" name="action">NORIU
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </div>
+        <h5>Aprašymas</h5>
+
+
+        <div class="justify-align">
+
+
+
+            <p>
+                <font size="3">Turbūt visi sutinka, kad pati žymiausia visų laikų lietuvė yra Barbora Radvilaitė. Nuo jos gimimo dienos praėjo jau penki šimtai metų, o moters asmenybė vis dar nepamiršta. Priešingai – Barbora apipinta keisčiausias gandais, legendomis, neįtikėtinomis istorijomis apie jos gyvenimą, meilę Žygimantui Augustui bei paslaptingą mirtį. Lenkų istorikas Zbigniew Kuchowicz penkių šimtų puslapių knygoje „Barbora Radvilaitė“ dalinasi savo provokuojančiu tyrimu apie legendinės asmenybės gyvenimą.
+                    Barboros Radvilaitės vardu vadiname miestų gatves, apie šią moterį statomi filmai, spektakliai, rašomos knygos.
+
+                    <br>
+                    Iš lenkų kalbos vertė Romualdas Petraitis
+                </font>
+            </p>
+
+
+        </div>
+    </div>
 
 </body>
 

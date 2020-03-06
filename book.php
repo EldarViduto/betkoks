a73099e9d69b1df989ac4a5c40b0248116ed120d
judu-dus <jurga.duseviciene@gmail.com>
2020-03-06 17:43:37 +0200

    ..

--- book.php
+++ book.php
@@ -3,11 +3,11 @@
 
 <body>
     
-<div>
+<div class=" col s12 s4 center-align">
 <img src="9786094840869.jpg">
     <h3>Barbora Radvilaitė</h3>
   <h4>Aprašymas</h4>
- <p align="right">Turbūt visi sutinka, kad pati žymiausia visų laikų lietuvė yra Barbora Radvilaitė. Nuo jos gimimo dienos praėjo jau penki šimtai metų, o moters asmenybė vis dar nepamiršta. Priešingai – Barbora apipinta keisčiausias gandais, legendomis, neįtikėtinomis istorijomis apie jos gyvenimą, meilę Žygimantui Augustui bei paslaptingą mirtį. Lenkų istorikas Zbigniew Kuchowicz penkių šimtų puslapių knygoje „Barbora Radvilaitė“ dalinasi savo provokuojančiu tyrimu apie legendinės asmenybės gyvenimą.
+ <p align="justify">Turbūt visi sutinka, kad pati žymiausia visų laikų lietuvė yra Barbora Radvilaitė. Nuo jos gimimo dienos praėjo jau penki šimtai metų, o moters asmenybė vis dar nepamiršta. Priešingai – Barbora apipinta keisčiausias gandais, legendomis, neįtikėtinomis istorijomis apie jos gyvenimą, meilę Žygimantui Augustui bei paslaptingą mirtį. Lenkų istorikas Zbigniew Kuchowicz penkių šimtų puslapių knygoje „Barbora Radvilaitė“ dalinasi savo provokuojančiu tyrimu apie legendinės asmenybės gyvenimą.
 Barboros Radvilaitės vardu vadiname miestų gatves, apie šią moterį statomi filmai, spektakliai, rašomos knygos. Regis Barbora nebuvo karvedė, realiai nevaldė šalies, bet vistiek tapo mūza daugeliui menininkų ir tyrinėjimo objektu žymiausiems istorikams.
 Žygimanto Augusto ir Barboros Radvilaitės meilės istorija išskirtinė ir iki pat šių dienų žadina smalsumą. Nei jų gyvenimo, nei dar vėlesniais laikais didikams ir karaliams nebuvo įprasta tuoktis iš meilės. Visgi ši pora nugalėjo tradicinį pasipriešinimą ir sukūrė šeimą vedini vien meilės jausmo. Tiesa, taip Barbora susilaukė ypatingai daug neapykantos iš visuomenės – moteris buvo vadinama ragana, kekše, intrigante ir kitais nemaloniais epitetais. O gal ji tokia ir buvo? – provokuoja autorius. Arba atvirkščiai, gal Barbora tiesiog tapo istorinių aplinkybių auka?
 Lenkų istorikas Zbigniew Kuchowicz pirmasis pabandė Barborą Radvilaitę vertinti objektyviai atsižvelgdamas į istorines aplinkybes bei tuo metu vyravusią politinę aplinką. „Barbora Radvilaitė“ – labai detalus ir įtraukiantis tyrimas, neįprasta istorinės asmenybės biografija, o taip pat ir meilės bei nepaprasto gyvenimo istorija.

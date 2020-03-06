7ca80ff9b62c0e1f0a9cff5d34e8427d48f4d78d
EldarViduto <eldarviduto@gmail.com>
2020-03-06 13:08:47 +0200

    Index positioningas

--- index.php
+++ index.php
@@ -13,59 +13,61 @@
     </div class="cols2 right">
     <br><br><br><br>
     <div class="right">
-    <a href="#">Register</a>
-    <button class="btn">LOGIN</button>
+      <a href="#">Register</a>
+      <button class="btn">LOGIN</button>
     </div>
-  </div>
-
-  <div>
-    <ul class="sidebox-border flow-text">
-      <li>
-        <a href="#">Zanras1</a>
-      </li>
-      <li>
-        <a href="#">Zanras2</a>
-      </li>
-      <li>
-        <a href="#">Zanras3</a>
-      </li>
-      <li>
-        <a href="#">Zanras4</a>
-      </li>
-      <li>
-        <a href="#">Zanras5</a>
-      </li>
-      <li>
-        <a href="#">Zanras6</a>
-      </li>
-    </ul>
   </div>
 
   <div class="row">
-    <div class="col s4">
-      <img src="https://picsum.photos/seed/1/200/300" alt="">
-      <p>Knygos pavadinimas</p>
-      <button>NORIU!</button>
+    <div class="col s12 m6 l3">
+      <ul class="sidebox-border flow-text">
+        <li>
+          <a href="#">Zanras1</a>
+        </li>
+        <li>
+          <a href="#">Zanras2</a>
+        </li>
+        <li>
+          <a href="#">Zanras3</a>
+        </li>
+        <li>
+          <a href="#">Zanras4</a>
+        </li>
+        <li>
+          <a href="#">Zanras5</a>
+        </li>
+        <li>
+          <a href="#">Zanras6</a>
+        </li>
+      </ul>
     </div>
-    <div class="col s4">
-      <img src="https://picsum.photos/seed/2/200/300" alt="">
-      <p>Knygos pavadinimas2</p>
-      <button>NORIU!</button>
-    </div>
-    <div class="col s4">
-      <img src="https://picsum.photos/seed/3/200/300" alt="">
-      <p>Knygos pavadinimas3</p>
-      <button>NORIU!</button>
-    </div>
-    <div class="col s4">
-      <img src="https://picsum.photos/seed/4/200/300" alt="">
-      <p>Knygos pavadinimas4</p>
-      <button>NORIU!</button>
-    </div>
-    <div class="col s4">
-      <img src="https://picsum.photos/seed/5/200/300" alt="">
-      <p>Knygos pavadinimas5</p>
-      <button>NORIU!</button>
+
+    <div class="row center-align">
+      <div class="col s3">
+        <img src="https://picsum.photos/seed/1/200/300" alt="">
+        <p>Knygos pavadinimas</p>
+        <button>NORIU!</button>
+      </div>
+      <div class="col s3">
+        <img src="https://picsum.photos/seed/2/200/300" alt="">
+        <p>Knygos pavadinimas2</p>
+        <button>NORIU!</button>
+      </div>
+      <div class="col s3">
+        <img src="https://picsum.photos/seed/3/200/300" alt="">
+        <p>Knygos pavadinimas3</p>
+        <button>NORIU!</button>
+      </div>
+      <div class="col s3">
+        <img src="https://picsum.photos/seed/4/200/300" alt="">
+        <p>Knygos pavadinimas4</p>
+        <button>NORIU!</button>
+      </div>
+      <div class="col s4">
+        <img src="https://picsum.photos/seed/5/200/300" alt="">
+        <p>Knygos pavadinimas5</p>
+        <button>NORIU!</button>
+      </div>
     </div>
   </div>
 </main>

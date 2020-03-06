146d65a27df7a6b4802d7c86bfe83069de091abf
EldarViduto <eldarviduto@gmail.com>
2020-03-06 08:34:25 +0200

    Headerio maketas

--- header.php
+++ header.php
@@ -17,4 +17,15 @@
 
 <body>
 
+<nav class="container">
+    <div class="nav-wrapper container">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required placeholder="Iveskite knygos pavadinima">
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
     <!-- Čia pasikartojanti visuose puslapiuose viršutinė dalis -->

2f3dde882b5bfcb16917d672c2ce0df5781697ad
Gest-VAK <gediminas.sturmas@gmail.com>
2020-03-09 21:02:14 +0200

    registracija veikia

--- register.php
+++ register.php
@@ -8,20 +8,18 @@
 
     if (isset($_GET["name"]) && $_GET["name"] != "") {
 
-        $Vardas = $_GET["user_name"];
-        $Pavardė = $_GET["user_surname"];
-        $email = $_GET["user_email"];
-        $password = $_GET["user_password"];
+        $name = $_GET["name"];
+        $surname = $_GET["surname"];
+        $email = $_GET["email"];
+        $password = $_GET["password"];
        
 
-        $sql = "INSERT INTO user_info (user_name, user_surname, user_email, user_pasword)
-        VALUES ('$Vardas', '$Pavardė', '$email', '$password')";
+        $sql = "INSERT INTO user_info (user_name, user_surname, user_email, user_password)
+        VALUES ('$name', '$surname', '$email', '$password')";
 
 
         if ($conn->query($sql) === true) {
-            echo "New record created successfully";
-
-            header("Location: index.php?status=success");
+            header("Location: register.php?status=success");
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -35,43 +33,39 @@
 
         <div class="row">
             <div class="input-field col s6">
-                <input placeholder="Vardas" id="Vardas" type="text" class="validate">
+                <input placeholder="Vardas" id="Vardas" type="text" class="validate" name="name">
                 <label for="Vardas">Vardas</label>
             </div>
             <div class="input-field col s6">
-                <input placeholder="Pavardė" id="Pavardė" type="text" class="validate">
+                <input placeholder="Pavardė" id="Pavardė" type="text" class="validate" name="surname">
                 <label for="Pavardė">Pavardė</label>
             </div>
         </div>
 
         <div class="row">
             <div class="input-field col s6">
-                <input id="email" type="email" class="validate">
+                <input id="email" type="email" class="validate" name="email">
                 <label for="email">El. paštas</label>
             </div>
 
             <div class="input-field col s6">
-                <input id="password" type="password" class="validate">
+                <input id="password" type="password" class="validate" name="password">
                 <label for="password">Slaptažodis</label>
             </div>
         </div>
 
-        <div class="row">
+        <!-- <div class="row">
             <div class="col s6">
-                <!-- <a href="#"><img src="images/user_img.jpg" alt="#"></a> -->
+                <a href="#"><img src="images/user_img.jpg" alt="#"></a>
                 <a href="https://picsum.photos/seed/2/200/3000"><button>Foto</button></a>
             </div>
-        </div>
-
-    </form>
-
-    <a href="user.php">
+        </div> -->
 
         <button class="btn waves-effect waves-light" type="submit" name="action">Patvirtinti
             <i class="material-icons right">send</i>
         </button>
 
-    </a>
+    </form>
 
 
 </div>

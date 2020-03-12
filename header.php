f1a7df940a0f77d90b14271694ad036ead2e1336
judu-dus <jurga.duseviciene@gmail.com>
2020-03-12 19:23:33 +0200

    .

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ include "conn.php";
 <body>
 
   <nav class="container">
-    <a href="index.php" class="brand-logo">Logo</a>
+    <a href="index.php" class="brand-logo">SKAITYK.lt</a>
     <ul class="right hide-on-med-and-down">
       <li><a href="user.php?id=<?php echo $row['id'] ?>"><i class="material-icons right">account_box</i>Guest</a></li>
       <!-- <?php
